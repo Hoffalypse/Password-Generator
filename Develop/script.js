e498fa00a5623ab6c0b0a401d79827ac828c0f3e
Bryan Wienhoff <106857564+Hoffalypse@users.noreply.github.com>
2022-07-04 17:58:20 -0600

    fixed string issue

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,16 @@
 // Define Global Variables
 var generateBtn = document.querySelector("#generate");
-var numbers = ("0"+"1"+"2"+"3"+"4"+"5"+"6"+"7"+"8"+"9");
-var specialChars = ( "~"+"`"+"!"+"@"+"#"+"$"+"%"+"^"+"&"+"*"+"("+")"+"-"+"_"+"+"+"="+"{"+"}"+"["+"]"+"|"+"/"+"?"+"."+"<"+">");
-var lowerCase = ("a"+"b"+"c"+"d"+"e"+"f"+"g"+"h"+"i"+"j"+"k"+"l"+"m"+"n"+"o"+"p"+"q"+"r"+"s"+"t"+"u"+"v"+"w"+"x"+"y"+"z");
-var upperCase = ("A"+"B"+"C"+"D"+"E"+"F"+"G"+"H"+"I"+"J"+"K"+"L"+"M"+"N"+"O"+"P"+"Q"+"R"+"S"+"T"+"U"+"V"+"W"+"X"+"Y"+"Z");
+var numbers = ("0123456789");
+var specialChars = ( "~`!@#$%^&()-_+={}[]|/?.<>");
+var lowerCase = ("abcdefghijklmnopqrstuvwxyz");
+var upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
 //user must hit "generate Password" before prompts appear  
 document.addEventListener("click", writePassword);
 
 // concatinate the strings based on user selection
 function writePassword() {
-    var numberChoice = prompt("Would you like to use Numbers? Y/N");
+    var numberChoice = prompt("Would you like to use Numbers?" + "\n" + "Choose Y or N");
         if (numberChoice === "Y") {
             var totalString = numbers;
         }
@@ -18,23 +18,48 @@ function writePassword() {
             var totalString = "";
           }
       
-    var specialChoice = prompt("Would you like to use Special Characters? Y/N");
+    var specialChoice = prompt("Would you like to use Special Characters?" + "\n" + "Choose Y or N");
         if(specialChoice === "Y") {
             totalString = totalString.concat(specialChars);
         }
 
-    var lowerChoice = prompt("Would you like to use Lower Case Letters? Y/N");
+    var lowerChoice = prompt("Would you like to use Lower Case Letters?" + "\n" + "Choose Y or N");
         if(lowerChoice === "Y") {
             totalString = totalString.concat(lowerCase);
         }
 
-    var upperChoice = prompt("Would you like to use Upper Case Letters? Y/N");
+    var upperChoice = prompt("Would you like to use Upper Case Letters?" + "\n" + "Choose Y or N");
         if(upperChoice === "Y") {
             totalString = totalString.concat(upperCase);
         }
+
+        if (totalString === ""){
+          alert("You must choose at least one character type, try again");
+          
+        }
       
-        
-}
+    //define password length
+    // var howMany = prompt("How many Characters would you like the Password to be?" + "\n" + "Choose from 8-128");
+    //     if (howMany > 7 && howMany < 129){
+    //       var goodLength = howMany;   
+    //     }
+    //       else {
+    //          alert("Incorrect Value Try again");
+    //       }
+    
+    // //math for random character selection   
+    // var output = "";    
+    //  for (var i = 0; 1<= goodLength; i++) {     
+    //     var password = totalString[ Math.floor(Math.random() * totalString.length)];
+    //     output += totalString.substring(password, password +1);
+    //  }      
+    
+    console.log(totalString);
+    
+
+
+
+  }
   
 
 
