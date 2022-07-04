355c4c02d5ba511b2a3cd69e00d69c8a0575417d
Hoffalypse <bhoff1980@gmail.com>
2022-07-04 02:58:27 -0600

    completed character prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,45 @@
-// Assignment Code
+// Define Global Variables
 var generateBtn = document.querySelector("#generate");
 var numbers = ("0"+"1"+"2"+"3"+"4"+"5"+"6"+"7"+"8"+"9");
 var specialChars = ( "~"+"`"+"!"+"@"+"#"+"$"+"%"+"^"+"&"+"*"+"("+")"+"-"+"_"+"+"+"="+"{"+"}"+"["+"]"+"|"+"/"+"?"+"."+"<"+">");
 var lowerCase = ("a"+"b"+"c"+"d"+"e"+"f"+"g"+"h"+"i"+"j"+"k"+"l"+"m"+"n"+"o"+"p"+"q"+"r"+"s"+"t"+"u"+"v"+"w"+"x"+"y"+"z");
 var upperCase = ("A"+"B"+"C"+"D"+"E"+"F"+"G"+"H"+"I"+"J"+"K"+"L"+"M"+"N"+"O"+"P"+"Q"+"R"+"S"+"T"+"U"+"V"+"W"+"X"+"Y"+"Z");
-var Y = true;
-var N = false;
 
-document.addEventListener("click", question);
-function question (){
-  var numberChoice = prompt("Would you like to use Numbers? Y/N");
+//user must hit "generate Password" before prompts appear  
+document.addEventListener("click", writePassword);
 
-  var specialChoice = prompt("Would you like to use Special Characters? Y/N");
-  
-  var lowerChoice = prompt("Would you like to use Lower Case Letters? Y/N");
-  
-  var upperChoice = prompt("Would you like to use Upper Case Letters? Y/N");
+// concatinate the strings based on user selection
+function writePassword() {
+    var numberChoice = prompt("Would you like to use Numbers? Y/N");
+        if (numberChoice === "Y") {
+            var totalString = numbers;
+        }
+          else {
+            var totalString = "";
+          }
+      
+    var specialChoice = prompt("Would you like to use Special Characters? Y/N");
+        if(specialChoice === "Y") {
+            totalString = totalString.concat(specialChars);
+        }
+
+    var lowerChoice = prompt("Would you like to use Lower Case Letters? Y/N");
+        if(lowerChoice === "Y") {
+            totalString = totalString.concat(lowerCase);
+        }
+
+    var upperChoice = prompt("Would you like to use Upper Case Letters? Y/N");
+        if(upperChoice === "Y") {
+            totalString = totalString.concat(upperCase);
+        }
+      
+        
 }
+  
+
+
+
+
 
 // Write password to the #password input
 // function writePassword() {
